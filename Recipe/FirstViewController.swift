3a171f9769a604b5e10a5651d2a0e4e0bfc3000d
iOS <Programmer@imac-di-ios.homenet.telecomitalia.it>
2015-11-20 10:44:01 +0100

    Initial Commit

---
+++ Recipe/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Recipe
+//
+//  Created by iOS on 20/11/15.
+//  Copyright © 2015 iOS. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
